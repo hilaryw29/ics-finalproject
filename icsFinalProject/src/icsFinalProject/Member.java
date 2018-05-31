709dfbcde6f8aa9a3a6b06d9e30c72eb7dd208b4
hilaryw29 <hilary_yhwg@outlook.com>
2018-05-31 14:49:52 -0400

    Created Member class and constructor

---
+++ icsFinalProject/src/icsFinalProject/Member.java
@@ -0,0 +1,25 @@
+package icsFinalProject;
+import java.util.*;
+
+public class Member {
+	String name;
+	double income;
+	double budget;
+	double expense;
+	double balance;
+	LinkedList<Account> accountlist;
+	double percentage;
+	
+	public Member (String name, double income, double budget, double percentage) {
+		this.name = name;
+		this.income = income;
+		this.budget = budget;
+		this.percentage = percentage;
+	}
+	
+	public boolean updateBalance (int accountId, double newBalance) {
+		
+	}
+	
+	
+}
