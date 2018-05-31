a26212f1646edeed6a5fd835716d92c523259126
squareCarrot <39807876+squareCarrot@users.noreply.github.com>
2018-05-31 15:01:05 -0400

    Add files via upload

---
+++ icsFinalProject/src/icsFinalProject/Transaction.java
@@ -0,0 +1,28 @@
+public class Transaction{
+	private int amount;
+	private String payer, payee;
+	private int payerAccountID, payeeAccountID;
+	private String description, date;
+	private int id;
+	
+	public Transaction(int amount, int payerAccountID, int payeeAccountID, String payer, String payee, String description, String date){
+		this.amount = amount;
+		this.payerAccountID = payerAccountID;
+		this.payeeAccountID = payeeAccountID;
+		this.payer = payer;
+		this.payee = payee;
+		this.description = description;
+		this.date = date;
+	}
+	
+	public Transaction(Transaction t1, int id){
+		this.amount = t1.amount;
+		this.payerAccountID = t1.payerAccountID;
+		this.payeeAccountID = t1.payeeAccountID;
+		this.payer = t1.payer;
+		this.payee = t1.payee;
+		this.description = t1.description;
+		this.date = t1.date;
+		this.id = id;
+	}
+}
