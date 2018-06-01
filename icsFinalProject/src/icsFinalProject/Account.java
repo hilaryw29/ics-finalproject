fd04bf10518f9ec50927de223ccc85e673ddc925
hilaryw29 <hilary_yhwg@outlook.com>
2018-06-01 13:46:03 -0400

    Created Account class and updated existing methods in Member class

---
+++ icsFinalProject/src/icsFinalProject/Account.java
@@ -0,0 +1,35 @@
+package icsFinalProject;
+
+public class Account {
+	String accountType;
+	int id;
+	double balance;
+	
+	public Account (String accountType, double balance) {
+		this.accountType = accountType;
+		this.balance = balance;
+		
+	}
+	
+	public Account(Account account, int id) {
+		this.id = id;
+		accountType = account.accountType;
+		balance = account.balance;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setBalance (double balance) {
+		this.balance = balance;
+	}
+}
