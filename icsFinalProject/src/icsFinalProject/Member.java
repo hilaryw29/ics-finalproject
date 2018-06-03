0f547dc36628d30e8463d426e58f4f0343d946b2
hilaryw29 <hilary_yhwg@outlook.com>
2018-06-03 15:01:04 -0400

    Updated Member class. Created assignBudget, checkBudget, updateBalance, addAccount, deleteAccount, listAccount. Commented questions/uncertainties in class.

--- icsFinalProject/src/icsFinalProject/Member.java
+++ icsFinalProject/src/icsFinalProject/Member.java
@@ -37,6 +37,26 @@ public class Member {
 		}
 		return null;
 	}
+	
+	private int findAccountIndex (int accountId) {
+		int bottom = 0;
+		int top = accountList.size()-1;
+		boolean found = false;
+		int middle;
+
+		while (bottom <= top && found == false) {
+			middle = (bottom + top) / 2;
+			if ((accountList.get(middle)).getId() == accountId) {
+				return middle;
+			} else if (accountId > (accountList.get(middle)).getId()) {
+				bottom = middle + 1;
+			} else {
+				top = middle - 1; 
+			}
+
+		}
+		return -1;
+	}
 
 	public boolean updateBalance(int accountId, double newBalance) {
 		Account account = findAccount(accountId);
@@ -48,8 +68,55 @@ public class Member {
 		}
 	}
 	
+	//Aren't the methods listed in the section below all getters and setters?
+	//Isn't this just like a setter? Why do we need to return a boolean? (When would it be false?)
 	public boolean assignBudget (double newBudget) {
-		
+		budget = newBudget;
+		return true;
 	}
+	
+	public double checkBudget () {
+		return budget;
+	}
+	
+	public void updateBalance (double newBalance) {
+		balance = newBalance;
+	}
+	
+	public boolean checkAccountBalance (String name, int accountId, double lowestBalance) {
+		Account account = findAccount (accountId);
+		if (account != null) {
+			if (account.getBalance() >= lowestBalance) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		// Should throw exception?
+		return false;
+	}
+	
+	//NOT DONE (are we supposed to generate the account id in this method?)
+//	public int addAccount (Account account) {
+//		accountList.add(account);
+//		return -1;
+//	}
+	
+	//Why do you need the name of the member as part of the parameters?
+	public boolean deleteAccount (String name, int accountId) {
+		int index = findAccountIndex (accountId);
+		
+		if (index != -1) {
+			accountList.remove(index);
+			return true;
+		}
+		return false;
+	}
+	
+	//Check, like this?
+	public LinkedList<Account> listAccount (){
+		return accountList;
+	}
+	
 
 }
