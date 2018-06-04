75aba057c8931aa5ee40d2038709c6f47d23d8c0
lefufang <lefu1298@gmail.com>
2018-06-04 16:24:02 -0400

    Added some text files

--- icsFinalProject/src/icsFinalProject/FileConstant.java
+++ icsFinalProject/src/icsFinalProject/FileConstant.java
@@ -2,5 +2,9 @@ package icsFinalProject;
 
 public class FileConstant {
 	public static final String MEMBERINFO = ".\\resources\\FamilyMembersInfo.txt";
-	public static final String PIN = ".\\resources\\pin.txt";
+	public static final String ACCOUNTS = ".\\resources\\Accounts.txt";
+	public static final String Entrance = ".\\resources\\Entrance.txt";
+	public static final String GOALS = ".\\resources\\Goals.txt";
+	public static final String TRANSACTIONS = ".\\resources\\Transactions.txt";
+	//public static final String PIN = ".\\resources\\pin.txt";
 }
