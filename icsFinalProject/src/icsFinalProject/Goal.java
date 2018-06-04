99f52213367b2fe63b0b37b2eef04b85c88f84cd
hilaryw29 <hilary_yhwg@outlook.com>
2018-06-04 15:14:16 -0400

    Created new Goal class

---
+++ icsFinalProject/src/icsFinalProject/Goal.java
@@ -0,0 +1,12 @@
+package icsFinalProject;
+
+public class Goal {
+	private double amount;
+	private String description;
+	
+	public Goal (double amount, String description) {
+		this.amount = amount;
+		this.description = description;
+	}
+
+}
