5febfac2f4c9ef56831298729e19fb37318c1bf5
11Willxo <39774646+11Willxo@users.noreply.github.com>
2018-06-05 13:23:29 -0400

    Add files via upload
    UHh

---
+++ icsFinalProject/src/icsFinalProject/FinalProduct/Adult.java
@@ -0,0 +1,26 @@
+package icsFinalProject;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Adult extends Member
+{
+	public Adult (String name, double income, double budget, double percentage)
+	{
+		super (name, income, budget, percentage);
+	}
+	
+	public boolean setGoal (Goal newGoal)
+	{
+		try
+		{
+		}
+		
+		catch (GoalException)
+		{
+		}
+		
+		
+	}
+}
