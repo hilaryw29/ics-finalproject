e73d6d3e50a63b5c5ad948c4627169383a8332d9
leeqz <leeqz@10.8.4.209>
2018-06-06 14:19:12 -0400

    update

--- icsFinalProject/src/icsFinalProject/FamilyBudgetManagement.java
+++ icsFinalProject/src/icsFinalProject/FamilyBudgetManagement.java
@@ -5,10 +5,26 @@ public class FamilyBudgetManagement {
 	private double houseHoldBalance;
 	private double minHouseHoldBalance;
 	private Thread DateManager;
-
+	private FamilyMemberList memberlist;
+	private TransactionList transactionList;
+	private RecurringBillsList billList;
 	public FamilyBudgetManagement(String fileName, String PIN) throws PINNotMatchException, FileNotFoundException, FileModifiedException {
 		
 	}
+	
+	public int addTransaction(Transaction transaction) throws AccountException {
+		updateBalance(transaction);
+		updateHoldBalance();
+	}
+	
+	private void updateBalance(Transaction transaction) throws AccountException {
+		FamilyMemberList.updateBalance(transaction);
+	}
+	
+	private void updateHoldBalance() {
+		houseHoldBalance=FamilyMemberList.getTotalBalance();
+	}
+	
 	
 	
 
