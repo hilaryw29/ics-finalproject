e9209d9296fa60f26b482e55a30f22c27db5acee
hilaryw29 <hilary_yhwg@outlook.com>
2018-06-08 01:15:06 -0400

    Update Family Member Menu

--- icsFinalProject/src/icsFinalProject/FamilyMemberMenu.java
+++ icsFinalProject/src/icsFinalProject/FamilyMemberMenu.java
@@ -1,6 +1,10 @@
 package icsFinalProject;
 
 public class FamilyMemberMenu extends Submenu {
+	final int SORT_INCOME = 1;
+	final int SORT_EXPENSE = 2;
+	final int ADD_MEMBER = 3;
+	final int SELECT_MEMBER = 4;
 	
 	public FamilyMemberMenu (String pass) {
 		super(pass);
@@ -11,12 +15,24 @@ public class FamilyMemberMenu extends Submenu {
 		int choice;
 		System.out.println("FAMILY MEMBER MENU");
 		System.out.println("Please enter a number 1-5 that corresponds to one of the following choices: ");
-		System.out.println("1. View family name");
-		System.out.println("2. Edit family name"); 
-		System.out.println("3. View low balance threshold");
-		System.out.println("4. Edit low balance threshold");
-		System.out.println("5. Edit pin");
+		System.out.println("1. Sort family member by income");
+		System.out.println("2. Sort family member by expense"); 
+		System.out.println("3. Add family member");
+		System.out.println("4. Select family member");
+		System.out.println("5. Back to main menu");
 		choice = intakeChoice(5);
+		
+		if (choice == SORT_INCOME) {
+
+		} else if (choice == SORT_EXPENSE) {
+			
+		} else if (choice == ADD_MEMBER) {
+			
+		} else if (choice == SELECT_MEMBER) {
+			
+		} else {
+			goBack();
+		}
 	}
 	
 	public void sortByIncome () {
@@ -25,7 +41,10 @@ public class FamilyMemberMenu extends Submenu {
 		sorted = family.sortMemberByIncome ();
 		
 		for (int i = 0; i < sorted.length; i ++) {
-			System.out.println (sorted [i]);
+			System.out.println ("Name: " + sorted [i].getName());
+			System.out.println("Income: "+ sorted[i].getName());
+			System.out.println ("");
 		}
+		displayMenu();
 	}
 }
