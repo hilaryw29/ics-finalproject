6f8de4ecd52888e1d5a0b6918db25e3b77190888
squareCarrot <39807876+squareCarrot@users.noreply.github.com>
2018-06-14 17:37:22 -0400

    Update Goal.java

--- icsFinalProject/src/icsFinalProject/Goal.java
+++ icsFinalProject/src/icsFinalProject/Goal.java
@@ -2,15 +2,19 @@ package icsFinalProject;
 
 import java.io.*;
 
+//Class description: the class helps family members set a “goal” to save money for
+//by creating an object and outputs a message when the goal is met.
 public class Goal implements Serializable{
 	private double amount;
 	private String description;
 	
+	//creates a goal with the given amount and description
 	public Goal (double amount, String description) {
 		this.amount = amount;
 		this.description = description;
 	}
 	
+	//accessors and mutators
 	public double getAmount () {
 		return amount;
 	}
@@ -19,6 +23,7 @@ public class Goal implements Serializable{
 		return description;
 	}
 	
+	//return whether the goal is achieved by intaking the given balance
 	public boolean goalAchieved (double balance) {
 		if (balance >= amount) {
 			return true;
