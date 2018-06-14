49cfb639a54d8da56d216ab0ae99c055ade6d1a1
squareCarrot <39807876+squareCarrot@users.noreply.github.com>
2018-06-14 17:43:22 -0400

    Update PINNotMatchException.java

--- icsFinalProject/src/icsFinalProject/PINNotMatchException.java
+++ icsFinalProject/src/icsFinalProject/PINNotMatchException.java
@@ -1,12 +1,18 @@
 package icsFinalProject;
+
+//Class description: the class inherits the Exception class and throws an exception
+//message when the PIN entered by the user is not matched to the correct PIN.
 public class PINNotMatchException extends Exception{
 	private String message;
 	private String enteredPIN;
+	
+	//constructs an exception with the PIN entered and the message
 	public PINNotMatchException(String message, String PIN) {
 		this.message = message;
 		this.enteredPIN = PIN;
 	}
 	
+	//Accessors
 	public String getEnteredPIN() {
 		return enteredPIN;
 	}
