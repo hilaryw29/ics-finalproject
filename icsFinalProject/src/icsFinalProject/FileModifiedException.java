e39600460758679d099d28e09d995dd683449e0d
squareCarrot <39807876+squareCarrot@users.noreply.github.com>
2018-06-14 17:32:01 -0400

    Update FileModifiedException.java

--- icsFinalProject/src/icsFinalProject/FileModifiedException.java
+++ icsFinalProject/src/icsFinalProject/FileModifiedException.java
@@ -2,16 +2,24 @@ package icsFinalProject;
 
 import java.io.IOException;
 
+//Class Description: the class iherits the Exception class and throws an exception message when
+//the input-file hash or the input-file syntax does not match.
 public class FileModifiedException extends IOException {
 	String fileName;
 	String errorType;
+	
+	//creates a new exception with the given filename and error type
 	public FileModifiedException(String fileName, String errorType) {
 		this.fileName = fileName;
 		this.errorType = errorType;
 	}
+	
+	//gives the filename with error
 	public String getFileName() {
 		return fileName;
 	}
+	
+	//gives the error type of the file
 	public String getErrorType() {
 		return errorType;
 	}
