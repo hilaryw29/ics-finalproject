0ca73e6f23e9bbe4d279c9a63631c91b0f0f7422
squareCarrot <39807876+squareCarrot@users.noreply.github.com>
2018-06-14 16:59:10 -0400

    Update Encryption.java

--- icsFinalProject/src/icsFinalProject/Encryption.java
+++ icsFinalProject/src/icsFinalProject/Encryption.java
@@ -20,29 +20,32 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SealedObject;
 import javax.crypto.spec.SecretKeySpec;
 
+//Class Description: the class encrypts the transactions or decrypts transactions. 
 public class Encryption {
 	private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
 	private static final String ALGORITHM1 = "AES";
 	
+	//the method encrypts the transactions
 	public static void encrypt(Serializable o, OutputStream out, byte[] password) throws IOException {
 	    try {
-	        SecretKeySpec key = new SecretKeySpec(password, ALGORITHM1);
-	        Cipher cipher = Cipher.getInstance(ALGORITHM);
-	        cipher.init(Cipher.ENCRYPT_MODE, key);
+	        SecretKeySpec key = new SecretKeySpec(password, ALGORITHM1);	//construct a new SecretKeySpec object
+	        Cipher cipher = Cipher.getInstance(ALGORITHM); 	//Create a Chipher object and getInstance(“AES”)
+	        cipher.init(Cipher.ENCRYPT_MODE, key);	//initialize the Cipher
 	        SealedObject sealedObject = new SealedObject(o, cipher);
 	        CipherOutputStream chipherOut = new CipherOutputStream(out, cipher);
 	        ObjectOutputStream objectOut = new ObjectOutputStream(chipherOut);
-	        objectOut.writeObject(sealedObject);
+	        objectOut.writeObject(sealedObject); 	//Convert the chipher.doFinal(the string) into String
 	        objectOut.close();
 	    } catch (IllegalBlockSizeException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
 	    	System.out.println("en");
 	    }
 	}
 
+	//the method decrypts the transactions
 	public static TransactionList decrypt(InputStream in, byte[] password) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
-		SecretKeySpec key = new SecretKeySpec(password, ALGORITHM1);
-	    Cipher cipher = Cipher.getInstance(ALGORITHM);
-	    cipher.init(Cipher.DECRYPT_MODE, key);
+		SecretKeySpec key = new SecretKeySpec(password, ALGORITHM1);	//construct a new SecretKeySpec object
+	    Cipher cipher = Cipher.getInstance(ALGORITHM);	//Create a Chipher object and getInstance(“AES”)
+	    cipher.init(Cipher.DECRYPT_MODE, key);	//initialize the Cipher
 	    CipherInputStream cipherIn = new CipherInputStream(in, cipher);
 	    try {
 	    	ObjectInputStream objectIn = new ObjectInputStream(cipherIn);
